30ff488e2625f8b8887a58f0423b53ad99d2659e
Fernanda <nanamyrita@gmail.com>
2025-10-21 18:41:57 -0300

    feat: carregar CVS, renomear colunas e limpar dados

--- case1_games.py
+++ case1_games.py
@@ -9,4 +9,27 @@ plt.rcParams["figure.figsize"] = (10, 6)
 OUTPUT_DIR = Path("outputs")
 OUTPUT_DIR.mkdir(exist_ok=True)
 
+CSV_PATH = Path("data/vgsales.csv")
+df = pd.read_csv(CSV_PATH)
 
+df.rename(columns={
+    "Nome" : "Name",
+    "Plataforma" : "Plataform",
+    "Ano" : "Year",
+    "Genero" : "Genre",
+    "Publisher" : "Publisher",
+    "Vendas Globais (milhões)" : "Global_Sales"
+}, inplace=True)
+
+df["Year"] = pd.to_numeric(df["Year"], errors="coerce")
+
+df.dropna(subset=["Year"], inplace=True) 
+df["Year"] = df["Year"].astype(int)
+
+missing_values = df.isnull().sum()
+print("Valores ausentes por coluna:\n", missing_values)
+
+df.dropna(inplace=True)
+
+
+print(df.head()) 
